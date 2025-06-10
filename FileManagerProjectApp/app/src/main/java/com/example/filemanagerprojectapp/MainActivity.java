d27112900fe7eed3275eb37096464adfacb7412c
Yumax83 <yumax83@gmail.com>
2025-06-10 17:32:21 +0300

    update1006

--- FileManagerProjectApp/app/src/main/java/com/example/filemanagerprojectapp/MainActivity.java
+++ FileManagerProjectApp/app/src/main/java/com/example/filemanagerprojectapp/MainActivity.java
@@ -1,24 +1,68 @@
 package com.example.filemanagerprojectapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.filemanagerprojectapp.fragments.CardFragment;
+import com.example.filemanagerprojectapp.fragments.InternalFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+       toggle.syncState(); //чтобы иконка появилась.
+
+        replaceFragment(new CardFragment());
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.nav_internal){
+                    replaceFragment(new InternalFragment());
+                  //   drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (item.getItemId() == R.id.nav_card){
+                    replaceFragment(new CardFragment());
+                 //    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (item.getItemId() == R.id.nav_about) {
+                    Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
         });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
