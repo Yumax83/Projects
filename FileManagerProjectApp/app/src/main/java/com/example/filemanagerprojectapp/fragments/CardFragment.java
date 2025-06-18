e4f57b277853f1cf18af081515bc5678ace169df
Yumax83 <yumax83@gmail.com>
2025-06-18 20:07:41 +0300

    update 1806

--- FileManagerProjectApp/app/src/main/java/com/example/filemanagerprojectapp/fragments/CardFragment.java
+++ FileManagerProjectApp/app/src/main/java/com/example/filemanagerprojectapp/fragments/CardFragment.java
@@ -1,6 +1,9 @@
 package com.example.filemanagerprojectapp.fragments;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -15,10 +18,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Environment;
 import android.provider.Settings;
+import android.text.format.Formatter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.filemanagerprojectapp.FileAdapter;
@@ -27,68 +34,107 @@ import com.example.filemanagerprojectapp.OnFileSelectedListener;
 import com.example.filemanagerprojectapp.R;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class CardFragment extends Fragment implements OnFileSelectedListener {
-    View view1;
-    File storage1;
+    View view;
+    File storage;
+
     private FileAdapter fileAdapter;
-    private RecyclerView recyclerView1;
+    private RecyclerView recyclerView;
 
     String data1;
+
+    String[] items = {"Details", "Rename", "Delete"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view1 = inflater.inflate(R.layout.fragment_card, container, false);
+        view = inflater.inflate(R.layout.fragment_card, container, false);
 
-        ImageView imgBackCard = view1.findViewById(R.id.img_back_card);
-        TextView tvPathHolderCard = view1.findViewById(R.id.tv_path_holder_card);
+        ImageView imgBackCard = view.findViewById(R.id.img_back_card);
+        TextView tvPathHolderCard = view.findViewById(R.id.tv_path_holder_card);
+
+//и третий -
+//// метод поиска корня карты.  его просто вызываем когда получаем доступ к карте
+///*    // ищем корень sd карты
+//    //Цель метода  Из стандартного пути вроде: /storage/XXXX-XXXX/Android/data/com.example.app/files/
+//    //получить: /storage/XXXX-XXXX То есть корневую директорию SD-карты.
+//    public File getSdCardRoot() {
+//        // Метод возвращает массив путей:
+//        //dirs[0] — внутренняя память: /storage/emulated/0/Android/data/...
+//        //dirs[1] — SD-карта (если есть): /storage/XXXX-XXXX/Android/data/...
+//        File[] dirs = ContextCompat.getExternalFilesDirs(getContext(), null);
+//        //хотим использовать второй путь — dirs[1].
+//        if (dirs != null && dirs.length > 1 && dirs[1] != null) {
+//            File sdPath = dirs[1]; //Сохраняем путь к SD-карте в переменную sdPath. На этом этапе он выглядит примерно так: /storage/XXXX-XXXX/Android/data/com.example.app/files/
+//            File parent;
+//            // Поднимаемся вверх, пока не найдём родителя с именем "storage"
+//            //цикл, который "шагает" вверх по дереву папок. Что мы делаем:
+//            //sdPath.getParentFile() — получаем родительскую папку.
+//            //Проверяем: имя папки не равно "storage" (всё ещё на SD-карте).
+//            while ((parent = sdPath.getParentFile()) != null && !parent.getName().equalsIgnoreCase("storage")) {
+//                sdPath = parent; //Если не дошли до /storage, то делаем шаг назад: sdPath = parent.
+//            }
+//            if (parent != null && parent.getName().equalsIgnoreCase("storage")) {
+//                return sdPath; //Когда мы дошли до /storage/XXXX-XXXX, возвращаем этот путь.
+//                //Пример как работает:
+//                //Шаг 1: /storage/XXXX-XXXX/Android/data/... → родитель = /storage/XXXX-XXXX/Android/data/
+//                //Шаг 2: → /storage/XXXX-XXXX/Android/
+//                //Шаг 3: → /storage/XXXX-XXXX/
+//                //Шаг 4: родитель = /storage/ → имя storage — стоп! Цикл завершён.
+//            }
+//        }
+//        return null;
 
         File[] externalDirs = ContextCompat.getExternalFilesDirs(getContext(), null);
         // Метод getExternalFilesDirs() Получает массив путей, где приложение может хранить файлы:
         // externalDirs[0] — это внутренняя память (например:
         // externalDirs[1] — это SD-карта, если она вставлена
 
-            if (externalDirs.length > 1 && externalDirs[1] != null) {
-                // Проверка: В массиве есть хотя бы 2 пути (length > 1) и Второй путь (SD-карта) реально существует (не null).
-                // если проще то строка читается так "Если в массиве есть второй путь, и он реально указывает на SD-карту — работаем с ним!"           // Внешняя SD-карта доступна. Действие:           storage = new File(externalDirs[1].getAbsolutePath()); // Запоминаешь путь к папке на SD-карте           cardTvPathFolder.setText("SD-карта: " + storage.getAbsolutePath());// Показываешь путь на экране           runtimePermission(); // вызов метода запроса разрешений       } else {           // Если SD-карты нет — используем внутреннюю           storage = new File(Environment.getExternalStorageDirectory().getAbsolutePath()); //Используем стандартный путь к внутренней памяти           cardTvPathFolder.setText("Встроенная память: " + storage.getAbsolutePath()); //Отображаем, что работаем с внутренней           runtimePermission(); // вызов метода запроса разрешений
+        if (externalDirs.length > 1 && externalDirs[1] != null) {
+            // Проверка: В массиве есть хотя бы 2 пути (length > 1) и Второй путь (SD-карта) реально существует (не null).
+            // если проще то строка читается так "Если в массиве есть второй путь, и он реально указывает на SD-карту — работаем с ним!"           // Внешняя SD-карта доступна. Действие:           storage = new File(externalDirs[1].getAbsolutePath()); // Запоминаешь путь к папке на SD-карте           cardTvPathFolder.setText("SD-карта: " + storage.getAbsolutePath());// Показываешь путь на экране           runtimePermission(); // вызов метода запроса разрешений       } else {           // Если SD-карты нет — используем внутреннюю           storage = new File(Environment.getExternalStorageDirectory().getAbsolutePath()); //Используем стандартный путь к внутренней памяти           cardTvPathFolder.setText("Встроенная память: " + storage.getAbsolutePath()); //Отображаем, что работаем с внутренней           runtimePermission(); // вызов метода запроса разрешений
 
-                // получаем доступ к внешней карте памяти
-                System.out.println("sss 1: " + storage1);
-                storage1 = new File(externalDirs[1].getAbsolutePath()); // Запоминаешь путь к папке на SD-карте
-                System.out.println("sss 2: " + storage1);
-                //tvPathHolderCard.setText("Внешняя SD-карта (список файлов и папок):");// Показываешь путь на экране
+            // получаем доступ к внешней карте памяти
+            System.out.println("sss 1: " + storage);
 
-                tvPathHolderCard.setText("SD-карта: " + storage1.getAbsolutePath());// Показываешь путь на экране
-                // вызов метода запроса разрешений
-                System.out.println("ddd 1: " + data1);
-                if (getArguments() != null) {
-                    data1 = getArguments().getString("path1");
-                    storage1 = new File(data1);
-                    System.out.println("ddd 2: " + data1);
+            //getAbsolutePath().split("/Android")[0]);
+           // getAbsolutePath().substring(0,19);
 
-                }
-                runtimePermission();
+            storage = new File(externalDirs[1].getAbsolutePath()); // Запоминаешь путь к папке на SD-карте
+            System.out.println("sss 2: " + storage);
+            //tvPathHolderCard.setText("Внешняя SD-карта (список файлов и папок):");// Показываешь путь на экране
 
-            } else {
-                // Если SD-карты нет — используем внутреннюю
-                storage1 = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
-                //Используем стандартный путь к внутренней памяти
+            tvPathHolderCard.setText("SD-карта: " + storage.getAbsolutePath());// Показываешь путь на экране
+            // вызов метода запроса разрешений
+           // System.out.println("ddd 1: " + data1);
+            if (getArguments() != null) {
+                data1 = getArguments().getString("path1");
+                storage = new File(data1);
+              //  System.out.println("ddd 2: " + data1);
+
+            }
+            runtimePermission();
+
+        } else {
+            // Если SD-карты нет — используем внутреннюю
+            storage = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+            //Используем стандартный путь к внутренней памяти
 //            if (getArguments() != null) {
 //                data = getArguments().getString("path");
 //                storage1 = new File(data);
 //            }
-                tvPathHolderCard.setText("Встроенная память: " + storage1.getAbsolutePath());
-                //Отображаем, что работаем с внутренней
-                runtimePermission(); // вызов метода запроса разрешений
-            }
+            tvPathHolderCard.setText("Встроенная память: " + storage.getAbsolutePath());
+            //Отображаем, что работаем с внутренней
+            runtimePermission(); // вызов метода запроса разрешений
+        }
 
 
-
-
-        return view1;
+        return view;
     }
 
     private void runtimePermission() {
@@ -150,38 +196,122 @@ public class CardFragment extends Fragment implements OnFileSelectedListener {
     }
 
     private void displayFiles() {
-        recyclerView1 = view1.findViewById(R.id.recycler_card);
-        recyclerView1.setHasFixedSize(true);
-        recyclerView1.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        recyclerView = view.findViewById(R.id.recycler_card);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
         List<File> fileList1 = new ArrayList<>();
-        fileList1.addAll(findFiles(storage1));
+        fileList1.addAll(findFiles(storage));
 
         fileAdapter = new FileAdapter(getContext(), fileList1, this);
-        recyclerView1.setAdapter(fileAdapter);
+        recyclerView.setAdapter(fileAdapter);
     }
 
     @Override
     public void onFileClicked(File file) {
         if (file.isDirectory()) {
-            Bundle bundle1 = new Bundle();
-            System.out.println("bbb: " + bundle1);
-            bundle1.putString("path1", file.getAbsolutePath());
-            System.out.println("bbb: " + bundle1);
-            // InternalFragment internalFragment = new InternalFragment();
-            //internalFragment.setArguments(bundle);
+            Bundle bundle = new Bundle();
+          //  System.out.println("bbb: " + bundle1);
+            bundle.putString("path1", file.getAbsolutePath());
+         //  System.out.println("bbb: " + bundle1);
+
             CardFragment cardFragment = new CardFragment();
-            cardFragment.setArguments(bundle1);
+            cardFragment.setArguments(bundle);
 
-
-            // requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, internalFragment).addToBackStack(null).commit();
             requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, cardFragment).addToBackStack(null).commit();
 
+        } else {
+            FileOpener.openFile(getContext(), file);
         }
     }
 
 
     @Override
     public void onFileLongClicked(File file) {
+        final Dialog optionDialog = new Dialog(getContext());
+        optionDialog.setContentView(R.layout.option_dialog);
+        optionDialog.setTitle("Select Options.");
+        ListView options = optionDialog.findViewById(R.id.list);
 
+        CustomAdapter1 customAdapter1 = new CustomAdapter1();
+        options.setAdapter(customAdapter1);
+
+        optionDialog.show();
+
+        options.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+
+                switch (selectedItem) {
+                    case "Details":
+                        AlertDialog.Builder detailDialog = new AlertDialog.Builder(getContext());
+                        detailDialog.setTitle("Details");
+                        final TextView details = new TextView(getContext());
+                        detailDialog.setView(details);
+                        Date lastModified = new Date(file.lastModified());
+                        SimpleDateFormat formatted = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                        String formattedDate = formatted.format(lastModified);
+
+//                        details.setText(String.format(
+//                                "File Name: " + file.getName() + "\n" + "Size: "
+//                                        + Formatter.formatFileSize(getContext(), file.length()) + "\n" + "Path: " + file.getAbsolutePath()
+//                                        + "\n" + "Last Modified: " + formattedDate));
+
+                        details.setText(String.format("File Name: %s\nSize: %s\nPath: %s\nLast Modified: %s",
+                                file.getName(), Formatter.formatShortFileSize(getContext(), file.length()), file.getAbsolutePath(), formattedDate));
+
+                        details.setPadding(70, 10, 10, 10);
+
+                        detailDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // optionDialog.cancel(); //закрывает два окна
+                                optionDialog.closeOptionsMenu(); //закрывает одно окно
+                            }
+                        });
+
+                        AlertDialog alertDialogDetails = detailDialog.create();
+                        alertDialogDetails.show();
+                        break;
+
+                }
+            }
+        });
+
+    }
+    class CustomAdapter1 extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View myView = getLayoutInflater().inflate(R.layout.option_layout, null);
+            TextView txtOptions = myView.findViewById(R.id.txt_option);
+            ImageView imgOptions = myView.findViewById(R.id.img_option);
+
+            txtOptions.setText(items[position]);
+
+            if (items[position].equals("Details")) {
+                imgOptions.setImageResource(R.drawable.baseline_info);
+            } else if (items[position].equals("Rename")) {
+                imgOptions.setImageResource(R.drawable.baseline_edit);
+            } else if (items[position].equals("Delete")) {
+                imgOptions.setImageResource(R.drawable.baseline_delete);
+            }
+            return myView;
+        }
     }
 }
