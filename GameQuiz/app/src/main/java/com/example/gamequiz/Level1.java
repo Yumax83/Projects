5748c47d7c265c7b472aa5db5244a4fd3f6f78fd
Yumax83 <yumax83@gmail.com>
2025-08-11 19:40:10 +0300

    update 1108

--- GameQuiz/app/src/main/java/com/example/gamequiz/Level1.java
+++ GameQuiz/app/src/main/java/com/example/gamequiz/Level1.java
@@ -16,9 +16,9 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.EdgeToEdge;
+
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
