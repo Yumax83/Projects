37a730a70d4271f84f7f85ec600e83cec5b0a0b2
Yumax83 <yumax83@gmail.com>
2025-08-13 13:26:30 +0300

    update 1308 p1

---
+++ DZQuizGame/app/src/main/java/com/example/dzquizgame/Array.java
@@ -0,0 +1,124 @@
+package com.example.dzquizgame;
+
+public class Array {
+
+    //массивы для второго уровня
+    //массивы для первого уровня
+
+    final int[] images2 = {
+            R.drawable.ch_zero,
+            R.drawable.ch_one,
+            R.drawable.ch_two,
+            R.drawable.ch_three,
+            R.drawable.ch_four,
+            R.drawable.ch_five,
+            R.drawable.ch_six,
+            R.drawable.ch_seven,
+            R.drawable.ch_eight,
+            R.drawable.ch_nine,
+    };
+    final int[] images1= {
+            R.drawable.aa_01,
+            R.drawable.bb_02,
+            R.drawable.kk_03,
+            R.drawable.oo_04,
+            R.drawable.pp_05,
+            R.drawable.rr_06,
+            R.drawable.ss_07,
+            R.drawable.tt_08,
+            R.drawable.uu_09,
+            R.drawable.cc_10,
+            R.drawable.chch_11,
+            R.drawable.shsh_12,
+            R.drawable.yiyi_13
+    };
+   // массивы для третьего уровня
+
+    final int[] images3 = {
+            R.drawable.level3_1,
+            R.drawable.level3_2,
+            R.drawable.level3_3,
+            R.drawable.level3_4,
+            R.drawable.level3_5,
+            R.drawable.level3_6,
+            R.drawable.level3_7,
+            R.drawable.level3_8,
+            R.drawable.level3_9,
+            R.drawable.level3_10,
+            R.drawable.level3_11,
+            R.drawable.level3_12,
+            R.drawable.level3_13,
+            R.drawable.level3_14,
+            R.drawable.level3_15,
+            R.drawable.level3_16,
+               };
+
+
+    //массивы для четвертого уровня
+
+    final int[] images44 = {
+            R.drawable.eb_1,
+            R.drawable.ir_1,
+            R.drawable.eb_2,
+            R.drawable.ir_2,
+            R.drawable.eb_3,
+            R.drawable.ir_3,
+            R.drawable.eb_4,
+            R.drawable.ir_4,
+            R.drawable.eb_5,
+            R.drawable.ir_5,
+            R.drawable.eb_6,
+            R.drawable.ir_6,
+            R.drawable.eb_7,
+            R.drawable.ir_7,
+            R.drawable.eb_8,
+            R.drawable.ir_8,
+            R.drawable.eb_9,
+            R.drawable.ir_9,
+            R.drawable.eb_10,
+            R.drawable.ir_10
+    };
+
+
+
+   final int[] choice = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+
+    final int[] images5 = {
+            R.drawable.amazonka_01,
+            R.drawable.nil_02,
+            R.drawable.missisipi_03,
+            R.drawable.yanscz_04,
+            R.drawable.ob_05,
+            R.drawable.huanh_06,
+            R.drawable.missuri_07,
+            R.drawable.mekong_08,
+            R.drawable.amur_09,
+            R.drawable.lena_10,
+            R.drawable.volga_11,
+    };
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
